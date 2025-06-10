ec1ec696b04defe7477d666549ac96fa80167a5f
shariffDevMern <sheriffgaming926@gmail.com>
2025-06-11 00:37:52 +0530

    dummy commit

--- src/store/loader-store.tsx
+++ src/store/loader-store.tsx
@@ -7,7 +7,7 @@ interface LoaderStore {
   setLoading: (loading: boolean) => void;
 }
 
-export const useLoaderStore = create<LoaderStore>((set) => ({
+export const useLoaderStore = create<LoaderStore>((set) => ({ 
   isLoading: false,
   setLoading: (loading: boolean) => set({ isLoading: loading }),
   isImageUploading: false,
