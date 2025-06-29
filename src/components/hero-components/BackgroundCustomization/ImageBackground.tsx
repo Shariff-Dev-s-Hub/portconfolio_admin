b26b5f617d78e219679cc38881c7a67a0e2786db
shariffDevMern <sheriffgaming926@gmail.com>
2025-06-29 11:18:06 +0530

    hero image uplaoder feature intitiated

---
+++ src/components/hero-components/BackgroundCustomization/ImageBackground.tsx
@@ -0,0 +1,13 @@
+import HeroImgUploader from "@/components/ui/hero-image-uploader";
+import { FormUtils } from "@/lib/interfaces";
+import React from "react";
+
+const ImageBackground: React.FC<{ formUtils: FormUtils }> = ({ formUtils }) => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[40svh] md:min-h-[30svh]">
+      <HeroImgUploader formUtils={formUtils} />
+    </div>
+  );
+};
+
+export default ImageBackground;
---
+++ src/components/ui/hero-image-uploader.tsx
@@ -0,0 +1,135 @@
+import { FormUtils } from "@/lib/interfaces";
+import React from "react";
+import styled from "styled-components";
+
+const HeroImgUploader: React.FC<{ formUtils: FormUtils }> = ({ formUtils }) => {
+  const { setValue = () => {}, watch } = formUtils;
+  return (
+    <>
+      {!watch("heroImageUrl") && (
+        <StyledWrapper>
+          <div className="input-div">
+            <input accept="image/*" className="input" name="file" type="file" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              fill="none"
+              stroke="currentColor"
+              className="icon"
+            >
+              <polyline points="16 16 12 12 8 16" />
+              <line y2={21} x2={12} y1={12} x1={12} />
+              <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3" />
+              <polyline points="16 16 12 12 8 16" />
+            </svg>
+          </div>
+        </StyledWrapper>
+      )}
+
+      <p className="text-gray-500 text-xs md:text-sm mt-8">
+        Upload better image for quality
+      </p>
+    </>
+  );
+};
+
+const StyledWrapper = styled.div`
+  .input-div {
+    position: relative;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    border: 2px solid #f8b13b;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    box-shadow: 0px 0px 100px #f8b13b, inset 0px 0px 10px #f8b13b,
+      0px 0px 5px rgb(255, 255, 255);
+    animation: flicker 2s linear infinite;
+  }
+
+  .icon {
+    color: #f8b13b;
+    font-size: 2rem;
+    cursor: pointer;
+    animation: iconflicker 2s linear infinite;
+  }
+
+  .input {
+    position: absolute;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    cursor: pointer !important;
+  }
+
+  @keyframes flicker {
+    0% {
+      border: 2px solid #f8b13b;
+      box-shadow: 0px 0px 100px #f8b13b, inset 0px 0px 10px #f8b13b,
+        0px 0px 5px rgb(255, 255, 255);
+    }
+
+    5% {
+      border: none;
+      box-shadow: none;
+    }
+
+    10% {
+      border: 2px solid #f8b13b;
+      box-shadow: 0px 0px 100px #f8b13b, inset 0px 0px 10px #f8b13b,
+        0px 0px 5px rgb(255, 255, 255);
+    }
+
+    25% {
+      border: none;
+      box-shadow: none;
+    }
+
+    30% {
+      border: 2px solid #f8b13b;
+      box-shadow: 0px 0px 100px #f8b13b, inset 0px 0px 10px #f8b13b,
+        0px 0px 5px rgb(255, 255, 255);
+    }
+
+    100% {
+      border: 2px solid #f8b13b;
+      box-shadow: 0px 0px 100px #f8b13b, inset 0px 0px 10px #f8b13b,
+        0px 0px 5px rgb(255, 255, 255);
+    }
+  }
+
+  @keyframes iconflicker {
+    0% {
+      opacity: 1;
+    }
+
+    5% {
+      opacity: 0.2;
+    }
+
+    10% {
+      opacity: 1;
+    }
+
+    25% {
+      opacity: 0.2;
+    }
+
+    30% {
+      opacity: 1;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+export default HeroImgUploader;
